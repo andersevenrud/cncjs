026e0a00d8a262b0b470d7bb1a314def84cf93c9
Anders Evenrud <andersevenrud@gmail.com>
2019-07-10 02:50:21 +0200

    Added download/upload util functions

--- src/engine/utils.ts
+++ src/engine/utils.ts
@@ -34,3 +34,57 @@ export const isNegative = (i: number): boolean => Object.is(-0, i) || i < 0;
  * Capitalizes word
  */
 export const capitalize = (s: string) => s.charAt(0).toUpperCase() + s.slice(1);
+
+/**
+ * Request browser to save a file
+ */
+export const requestSaveFile = (blob: Blob, filename: string) => {
+  if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+    window.navigator.msSaveOrOpenBlob(blob, filename);
+    return;
+  }
+
+  const file = URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  a.setAttribute('style', 'display: none');
+  a.setAttribute('href', file);
+  a.setAttribute('download', filename);
+
+  document.body.appendChild(a);
+  a.click();
+
+  window.URL.revokeObjectURL(file);
+  document.body.removeChild(a);
+};
+
+/**
+ * Request browser to open file
+ */
+export const requestLoadFile = (): Promise<string> => new Promise((resolve, reject) => {
+  const input = document.createElement('input');
+  input.type = 'file';
+
+  input.addEventListener('change', (ev: Event) => {
+    if (input.files!.length > 0) {
+      const reader = new FileReader();
+      reader.onload = (evt) => {
+        if (reader.error) {
+          reject(reader.error);
+        } else if (reader.readyState === 2) {
+          resolve(reader.result as string);
+        }
+      };
+      reader.onerror = evt => {
+        reject(evt);
+      };
+
+      reader.readAsText(input.files![0]);
+    }
+  });
+
+  input.addEventListener('error', (ev) => {
+    reject(ev);
+  });
+
+  input.click();
+});
